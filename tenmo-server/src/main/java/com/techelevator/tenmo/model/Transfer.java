32bb6424323cc488dd8b55a72ebfa986092d523d
Muggyfox1 <47337746+Muggyfox1@users.noreply.github.com>
2022-03-11 13:50:46 -0500

    valid transfer added

--- tenmo-server/src/main/java/com/techelevator/tenmo/model/Transfer.java
+++ tenmo-server/src/main/java/com/techelevator/tenmo/model/Transfer.java
@@ -1,16 +1,27 @@
 package com.techelevator.tenmo.model;
 
+import javax.validation.constraints.DecimalMin;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
 import java.math.BigDecimal;
 
 public class Transfer {
     //Instance Properties
     private long transferId;
+
+    @Min(1)
+    @Max(2)
     private int transferType;
+
+    @Min(1)
+    @Max(3)
     private int transferStatus;
     private long accountFrom;
     private String userNameFrom;
     private long accountTo;
     private String userNameTo;
+
+    @DecimalMin("0.01")
     private BigDecimal amount;
 
     //Getters and Setters
